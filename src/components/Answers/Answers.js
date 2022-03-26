4f6741b268466561a933ab6d6c4bb7ae3fd356c8
thakurSaad <thakursaad613@gmail.com>
2022-03-26 18:45:42 +0600

    check

--- src/components/Answers/Answers.js
+++ src/components/Answers/Answers.js
@@ -4,15 +4,18 @@ import "./Answers.css";
 const Answers = () => {
   return (
     <div className="answers">
-      <h1>From Answers</h1>
+      <h1 className="main-sub-heading">Frequently Asked Question</h1>
       <div className="answer">
-        <h4>Question : </h4>
+        <h4>Question : How react works?</h4>
+        <p>Answer :</p>
       </div>
       <div className="answer">
-        <h4>Question : </h4>
+        <h4>Question : Difference between props and state</h4>
+        <p>Answer :</p>
       </div>
       <div className="answer">
-        <h4>Question : </h4>
+        <h4>Question : How useState() works?</h4>
+        <p>Answer :</p>
       </div>
     </div>
   );
